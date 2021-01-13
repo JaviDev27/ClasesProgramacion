171f49a0d27ea341e1a609d3c0cb386bf91f9351
Javier Manobanda <51manobandajavier@gmail.com>
2021-01-13 10:59:05 -0500

    Se hace un clone de repositorio y se revisa jsx

---
+++ CursoReact_JS/platzi-badges-master/src/index.js
@@ -0,0 +1,9 @@
+import React from   'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1> Hello, Platzi HOLAAA</h1>
+const container = document.getElementById('app');
+
+//ReactDOM.render(--que--, --donde--)
+
+ReactDOM.render(element,container);
