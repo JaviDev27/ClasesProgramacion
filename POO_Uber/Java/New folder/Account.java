5337dfb44ae09ac07534a1bbae7e4e2023a69148
Javier <51manobandajavier@gmail.com>
2021-01-27 15:39:23 -0500

    nuevo archivo

---
+++ POO_Uber/Java/New folder/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    int id;
+    String name ;
+    String document;
+    String email;
+    String password;
+    
+    public Account(String name, String document) {
+        this.name = name;
+        this.document = document;
+    }
+}
