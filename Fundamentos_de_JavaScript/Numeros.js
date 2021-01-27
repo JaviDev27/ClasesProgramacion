3362589c4e0bf565a85346f812e0a7add258f88e
Javier Manobanda <51manobandajavier@gmail.com>
2021-01-27 10:01:19 -0500

    numeros strings en js

---
+++ Fundamentos_de_JavaScript/Numeros.js
@@ -0,0 +1,10 @@
+var edad = 27;
+edad += 1;
+console.log(`${edad}`);
+var peso = 75;
+peso -= 1;
+console.log(peso);
+var precioVino = 200.3;
+var total = Math.round(precioVino * 100 * 3) / 100;
+console.log(total.toFixed(2));
+var total2 = parseFloat(total);
---
+++ Fundamentos_de_JavaScript/String.js
@@ -0,0 +1,21 @@
+var nombre;
+
+nombre = "Javier";
+var nombreMayuscuylas = nombre.toUpperCase();
+console.log(nombreMayuscuylas);
+var nombreMinusculas = nombre.toLowerCase();
+
+var primeraletraDelNombre = nombre.charAt(0);
+
+var numCaracteres = nombre.length;
+
+var estudiantes = [2, 3, 4, 42, 3333];
+
+for (var estudiante of estudiantes) {
+  console.log(estudiante);
+}
+
+var str = nombre.substring(0, 4);
+var lastChartName = nombre.charAt(nombre.length - 1);
+
+/// Numeros
