f7339de65e81f6f44018802d1afc4d3852835696
Javier Manobanda <51manobandajavier@gmail.com>
2021-02-09 08:29:30 -0500

    Se agrega ejercicio del morral

--- POO_con_Python/AlgoritmosDeBusquedaOrganizacion/ordenamiento_por_mezcla.py
+++ POO_con_Python/AlgoritmosDeBusquedaOrganizacion/ordenamiento_por_mezcla.py
@@ -57,3 +57,11 @@ if __name__ == '__main__':
     lista_ordenada = ordenamiento_por_mezcla(lista)
 
     print(lista_ordenada)
+
+    def my_func(x):
+        respuesta = 0
+        for i in range(2000):
+            respuesta += 1
+
+        return respuesta
+    print(my_func(0))
---
+++ POO_con_Python/morral.py
@@ -0,0 +1,22 @@
+def morral(tamano_moral, pesos, valores, n):
+
+    print(f'moral es {tamano_moral}, numero items es {n}')
+    # Caso base
+    if n == 0 or tamano_moral == 0:
+        return 0
+
+    if pesos[n-1] > tamano_moral:
+        return morral(tamano_moral, pesos, valores, n-1)
+
+    return max(valores[n-1]+morral(tamano_moral - pesos[n-1], pesos, valores, n-1),
+               morral(tamano_moral, pesos, valores, n-1))
+
+
+if __name__ == '__main__':
+    valores = [60, 100, 120]
+    pesos = [10, 20, 30]
+    tamano_moral = 30
+    n = len(valores)
+
+    resultado = morral(tamano_moral, pesos, valores, n)
+    print(resultado)
